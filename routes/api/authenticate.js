a6dce43abc2ef80b6a086886d9d966b9d4fad9c2
akhiyarwaladi <akiyar18@gmail.com>
2017-05-25 12:32:20 +0700

    balik dulu

--- routes/api/authenticate.js
+++ routes/api/authenticate.js
@@ -1,47 +1,52 @@
 var async = require('async'),
 keystone = require('keystone');
-
+var User = keystone.list('User');
 
 exports.signin = function (req, res) {
+	data = (req.method == 'POST') ? req.body : req.query;
+ 	if (!data.username || !data.password) return res.apiResponse({ success: false });
   
-  if (!req.body.username || !req.body.password) return res.json({ success: false });
-  
-  keystone.list('User').model.findOne({ email: req.body.username }).exec(function(err, user) {
-    
-    if (err || !user) {
-      return res.json({
-        success: false,
-        session: false,
-        message: (err && err.message ? err.message : false) || 'Sorry, there was an issue signing you in, please try again.'
-      });
-    }
-    
-    keystone.session.signin({ email: user.email, password: req.body.password }, req, res, function(user) {
-      
-      return res.json({
-        success: true,
-        session: true,
-        date: new Date().getTime(),
-        userId: user.id,
-		token: "SECRET_API_KEY"
-      });
-      
-    }, function(err) {
-      
-      return res.json({
-        success: true,
-        session: false,
-        message: (err && err.message ? err.message : false) || 'Sorry, there was an issue signing you in, please try again.'
-      });
-      
-    });
-    
-  });
+	User.model.findOne({ email: data.username }).exec(function(err, user) {
+
+		if (err || !user) {
+			res.apiResponse({
+				error: true,
+				success: false,
+				session: false,
+				message: (err && err.message ? err.message : false) || 'Sorry, there was an issue signing you in, please try again.'
+			});
+		}
+
+		keystone.session.signin({ email: user.email, password: data.password }, req, res, function(user) {	  
+			res.apiResponse({
+				error: false,
+				success: true,
+				session: true,
+				date: new Date().getTime(),
+				userId: user.id,
+				username: "admin",
+				apiKey: "SECRET_API_KEY"
+			});
+		  
+		}, function(err) {
+		  
+			res.apiResponse({
+				error: true,
+				success: true,
+				session: false,
+				message: (err && err.message ? err.message : false) || 'Sorry, there was an issue signing you in, please try again.'
+			});
+		  
+		});
+
+	});
 }
 
 // you'll want one for signout too
 exports.signout = function (req, res) {
+
 	keystone.session.signout(req, res, function() {
-		res.json({ 'signedout': true });
+		res.apiResponse({ 'signedout': true });
 	});
+
 }
